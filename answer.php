0b80ca4eb8e6f84a1865e590dbfc0df47af59ad6
seisyo <seisyo1234@gmail.com>
2015-04-18 13:11:45 +0800

    A simple login system, account = seisyo1234, password = 12345678.

---
+++ answer.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Answer Page</title>
+</head>
+<body>
+	<hr>
+	<center>
+		<h3>登入結果</h3>
+		<hr>
+		<?php
+		$checking_account=$_POST["your_account"]."<br>";
+		$checking_password=$_POST["your_password"]."<br>";
+		if ($checking_account == "seisyo1234" and $checking_password == 12345678) {
+			echo "登入成功！";
+		}
+		else {
+			echo "輸入資料有誤！";
+		}
+		?>
+	</center>
+</body>
+</html>
