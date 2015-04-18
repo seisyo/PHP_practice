b52ddc7b5381df2840876c8dbe85b1ff44583f00
seisyo <seisyo1234@gmail.com>
2015-04-19 00:00:57 +0800

    connect to the localhost server, but it's something wrong.

--- answer.php
+++ answer.php
@@ -15,7 +15,7 @@
 			$SESSION["password"]=$_POST["your_password"]."<br>";
 
 			//資料庫設定
-			$db_server = "localhost";
+			$db_server = "127.0.0.1";
 			$db_name = "member";
 			$db_user = "root";
 			$db_password = "12345678";
@@ -23,6 +23,9 @@
 			//資料庫連線
 			if (!@mysql_connect($db_server, $db_name, $db_user, $db_password)){
 				die("無法連線！");
+			}
+			else{
+				echo "成功連線！";
 			}
 			mysql_query("SET NAMES utf8");
 
