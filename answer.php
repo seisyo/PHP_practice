c59290d94b5d6730da052ca1ed2c6d22a5aaba34
seisyo <seisyo1234@gmail.com>
2015-04-19 16:36:37 +0800

    The login can run!!!

--- answer.php
+++ answer.php
@@ -11,8 +11,8 @@
 		<?php
 			session_start();
 			//頁面輸入帳號及密碼紀錄
-			$SESSION["account"]=$_POST["your_account"]."<br>";
-			$SESSION["password"]=$_POST["your_password"]."<br>";
+			$_SESSION["accountIn"]=$_POST["your_account"];
+			$_SESSION["passwordIn"]=$_POST["your_password"];
 			
 			//資料庫設定
 			$db_server = "127.0.0.1";
@@ -22,19 +22,31 @@
 
 			//資料庫連線
 			if (!mysql_connect($db_server, $db_user, $db_password)){
-				//var_dump(mysql_connect($db_server, $db_user, $db_password));
 				die("無法連線！");
 			}
 			else{
-				//var_dump(mysql_connect($db_server, $db_user, $db_password));
 				echo "成功連線！";
 			}
 			mysql_query("SET NAMES utf8");
 
-			$temp = mysql_query("select * from `member`.`accpass` where `id` = 'seisyo1234';");
+			//資料去搜尋此帳號資料
+			$temp = mysql_query("select * from `member`.`accpass` where `account` = '".$_SESSION["accountIn"]."';");
 			$resultarr = mysql_fetch_row($temp);
-			print_r($resultarr);
-			//var_dump(mysql_query("select `password` from `member`.`accpass` where `account` = '".$SESSION["account"]."';"));
+			//print_r($resultarr);
+			
+			//判斷帳號及密碼是否符合
+			if ($_SESSION["accountIn"] != null && $_SESSION["passwordIn"] != null && (String)$resultarr[1] == (String)$_SESSION["accountIn"] && (String)$resultarr[2] == (String)$_SESSION["passwordIn"]){
+				$_SESSION["username"] = $_SESSION["accountIn"];
+				echo "登入成功！";
+			}
+			else{
+				// var_dump($_SESSION["accountIn"] != null);
+				// var_dump($_SESSION["passwordIn"] != null);
+				// var_dump((String)$resultarr[1] == (String)$_SESSION["accountIn"]);
+				// var_dump((String)$resultarr[2] == (String)$_SESSION["passwordIn"]);
+				echo "登入失敗！";
+			}
+			session_unset();
 		?>
 	</center>
 </body>
